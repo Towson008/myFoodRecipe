250f3d27e0540b5d558e718d7641e84e584e4d89
OLOWOMOJUORE TOSIN <federal@OLOWOMOJUOREs-MacBook-Air.local>
2022-05-18 10:40:01 +0100

    Updating codes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Pages from "./pages/Pages";
+import Category from "./components/Category";
 
 function App() {
   return (
     <div className="App">
       <h1>Hi There</h1>
+      <Category />
       <Pages />
     </div>
   );
